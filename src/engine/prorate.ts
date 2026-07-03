dee220c3beaef17832a9c7ba71561bf5330f8ada
hjasani-bmc <hjasani@bluematterconsulting.com>
2026-07-03 21:41:02 +0530

    Delete src directory

--- src/engine/prorate.ts
+++
@@ -1,7 +0,0 @@
-/** Pro-rated target incentive per rep = annual TI x (months / 12). */
-export function proratedTargetIncentive(
-  annualTargetIncentive: number,
-  durationMonths: number,
-): number {
-  return annualTargetIncentive * (durationMonths / 12)
-}
